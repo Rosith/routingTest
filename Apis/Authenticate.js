266b9d8d71ee85a91af99d1189c696babc3250d4
Rosith <rosith.55@gmail.com>
2017-03-01 15:21:40 +0530

    User registration modified
    Save command was missing. Added new routs to get single and all users

--- Apis/Authenticate.js
+++ Apis/Authenticate.js
@@ -1,14 +1,38 @@
 var User = require("../Model/User")
 
 module.exports.controller = function (apiRouts){
+    ///Register new user
     apiRouts.post("/signup", function(req,resp){
-        if (!req.body.name || !req.body.password) {
-            resp.json({success: false, msg: 'Please pass name and password.'});
+        if (!req.body.fullName && !req.body.password) {
+            resp.json({ success: false, msg: 'Please pass name and password.' });
         } else {
             var newUser = new User({
                 FullName: req.body.fullName,
                 UserName: req.body.userName,
                 Password: req.body.password
+            });
+            newUser.save();
+        }
+    });
+    
+    ///Get all saved users
+    apiRouts.get("/user", function (req, resp) {
+        User.find({}, function (err, users) {
+            if (err)
+                throw err;
+            resp.json(users);
+        });
+    });
+    ///Get users by id
+    apiRouts.get("/user/:user_name", function (req, resp) {
+        if (req.params && req.params.user_name) {
+            User.find({ UserName: req.params.user_name }, function (err, users) {
+                if (err)
+                    throw err;
+                if (users && users.length > 0)
+                    resp.json(users);
+                else
+                    resp.json({message: "No User to show"});
             });
         }
     });
