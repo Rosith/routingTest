eedd2f47e84fe66fb8291f06747e04cb1bd4fab8
Rosith <rosith.55@gmail.com>
2017-03-27 18:26:41 +0530

    mongo db connetion change
    mongo db connetion change

--- Configurations/webconfig.js
+++ Configurations/webconfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'database':'mongodb://localhost/testRoute',
+    'database':'mongodb://rosith:passwordmongo@cluster0-shard-00-00-szgam.mongodb.net:27017,cluster0-shard-00-01-szgam.mongodb.net:27017,cluster0-shard-00-02-szgam.mongodb.net:27017/routingtest?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
     'port':'8084',
     'secretKey':"keyTestRoute"
 }
